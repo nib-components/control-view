50d0ab3c452cb646c354b7f5d15a019a704752e1
James Newell <j.newell@nib.com.au>
2014-08-14 13:44:53 +1000

    added `isLoading()` and `setLoading()` methods so we can display a loading state without affecting the state

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var init = ControlView.prototype.init;
 ControlView.prototype.init = function() {
   init.call(this);
 
+  //initialise the feedback tip
   this.feedbackTarget = this.el.querySelector('.js-feedback-icon');
   if (this.feedbackTarget) {
     this.tip = new Tip();
@@ -30,6 +31,29 @@ var hideMessage = ControlView.prototype.hideMessage;
 ControlView.prototype.hideMessage = function() {
   hideMessage.call(this);
   if (this.feedbackTarget) this.tip.hide();
+  return this;
+};
+
+/**
+ * Get whether the control is waiting on something to load
+ * @returns {boolean}
+ */
+ControlView.prototype.isLoading = function() {
+  return this.el.classList.contains('is-loading');
+};
+
+/**
+ * Set whether the control is waiting on something to load
+ * @param   {Boolean} loading
+ * @returns {ControlView}
+ */
+ControlView.prototype.setLoading = function(loading) {
+  console.log('loading');
+  if (loading) {
+    this.el.classList.add('is-loading');
+  } else {
+    this.el.classList.remove('is-loading');
+  }
   return this;
 };
 
